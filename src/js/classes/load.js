9504740508f32389906b669d252af29f44086c79
Aleksandar Radosavljevic <104073687+AlexPray@users.noreply.github.com>
2022-04-21 09:30:09 +0200

    Update load.js

--- src/js/classes/load.js
+++ src/js/classes/load.js
@@ -1,26 +1,28 @@
 "use strict" 
 
-window.onload = function () {
-    let kanban_board_container = document.querySelector("#kanban-board-container")
-    kanban_board_container.innerHTML = `${localStorage.getItem('kanban_board_container')}`
+if (localStorage.getItem("kanban_board_container") !== null) {
+    window.onload = function () {
 
-    let task_list = document.querySelectorAll(".task")
-
-    task_list.forEach(function (task) {
-        remove_event_hinzufuegen(task);
-        editable_event_hinzufuegen(task);
-        change_prio_event_hinzufuegen(task)
-        drag_and_drop_event_hinzufuegen();
-    }); 
-
-    let add_task_buttons = document.querySelectorAll(".add-task-button");
-
-    add_task_buttons.forEach(e => {
-        e.addEventListener("click", e => {
-            html_generieren(e);
+        let kanban_board_container = document.querySelector("#kanban-board-container")
+        kanban_board_container.innerHTML = `${localStorage.getItem('kanban_board_container')}`
+    
+        let task_list = document.querySelectorAll(".task")
+    
+        task_list.forEach(function (task) {
+            remove_event_hinzufuegen(task);
+            editable_event_hinzufuegen(task);
+            change_prio_event_hinzufuegen(task)
             drag_and_drop_event_hinzufuegen();
-            speichern()
-        })
-    });
-
+        }); 
+    
+        let add_task_buttons = document.querySelectorAll(".add-task-button");
+    
+        add_task_buttons.forEach(e => {
+            e.addEventListener("click", e => {
+                html_generieren(e);
+                drag_and_drop_event_hinzufuegen();
+                speichern()
+            })
+        });
+    }
 }
